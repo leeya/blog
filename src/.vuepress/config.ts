3564a09ceb6cfc61128962aff4d7708a3e8e86b2
leeya <leeya_bao@msn.com>
2025-07-22 10:48:58 +0800

    修改标题和描述

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "Leeya's Blog",
+  description: "Leeya's personal blog, sharing knowledge and experiences.",
 
   theme,
 
